4fc44291944404b8e4f455d6ad1da83aaba960f8
yogeswar.manerikar@gmail.com <yogeswar.manerikar@gmail.com>
2021-05-19 22:03:24 +0530

    admin to html

--- Dashborad/Plant_admin/includes/footer.php
+++
@@ -1,9 +0,0 @@
-            <div class="hk-footer-wrap container">
-                <footer class="footer">
-                    <div class="row">
-                        <div class="col-md-6 col-sm-12">
-<p>Developed By<a href="" class="text-dark">Goa University</a> ©2021</p>
-                        </div>
-                    </div>
-                </footer>
-            </div>
