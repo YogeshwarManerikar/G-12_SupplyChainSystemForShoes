3f17d211b5549d7298d88dd9e283441c32b85a4e
Himanshul20 <himanshul.keshri6@gmail.com>
2021-07-06 18:12:26 +0530

    upload

--- batashowroom(app)/batacampany/bata/model/tracking_system.py
+++ batashowroom(app)/batacampany/bata/model/tracking_system.py
@@ -2,10 +2,16 @@ from django.db import models
 
 
 class Tracking_reports(models.Model):
-    tracking_id= models.IntegerField()
+    tracking_id = models.IntegerField()
     date = models.DateField(default=None)
     track_status = models.CharField(max_length=999)
 
+    @staticmethod
+    def get_track_status(tracking_id):
+        try:
+            return Tracking_reports.objects.get(tracking_id=tracking_id)
+        except:
+            return False
 
     class Meta:
         db_table = "Tracking_reports"
