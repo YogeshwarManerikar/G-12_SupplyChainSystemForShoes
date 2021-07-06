3faf55ffdc517d738b77f38a9864f76dbcde75e1
Himanshul20 <himanshul.keshri6@gmail.com>
2021-07-06 18:42:41 +0530

    upload

--- batashowroom(app)/batacampany/bata/model/raw_material_category.py
+++ batashowroom(app)/batacampany/bata/model/raw_material_category.py
@@ -1,16 +1,29 @@
 from django.db import models
 
-from .bata_plant import BATA_PLANT
+from .Raw_product import Raw_Product
+from .product import Product
 
 
-class Raw_Product(models.Model):
-    name = models.CharField(max_length=50)
-    Material_id = models.CharField(max_length=30,default='none')
-    price = models.IntegerField(default=0)
-    user_id = models.ForeignKey(BATA_PLANT, on_delete=models.CASCADE, default=0)
-    pro
+class Raw_Product_category(models.Model):
+    Polyurethane = models.IntegerField(max_length=50,default=None)
+    Rubber = models.IntegerField(max_length=50,default=None)
+    Dye = models.IntegerField(max_length=50,default=None)
+    Packaging_Material = models.IntegerField(max_length=50,default=None)
+    GUM = models.IntegerField(max_length=50,default=None)
+    PVC_Sole = models.IntegerField(max_length=50,default=None)
+    TPR = models.IntegerField(max_length=50,default=None)
+    Hard_Thread = models.IntegerField(max_length=50,default=None)
+    Rexene = models.IntegerField(max_length=50,default=None)
+    Clips = models.IntegerField(max_length=50,default=None)
+    Color = models.IntegerField(max_length=50,default=None)
+    Price = models.IntegerField(max_length=50,default=None)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE, default=0)
+    raw_product=models.ForeignKey(Raw_Product, on_delete=models.CASCADE, default=0)
+
+
+
     def __str__(self):
         return self.name
 
     class Meta:
-        db_table = "RawMaterial_Product"
+        db_table = "RawMaterial_categories"
