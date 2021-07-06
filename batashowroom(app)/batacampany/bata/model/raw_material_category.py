8bcb040e4ead2e7fc339145633d6958374418d85
Himanshul20 <himanshul.keshri6@gmail.com>
2021-07-06 18:57:00 +0530

    upload

--- batashowroom(app)/batacampany/bata/model/raw_material_category.py
+++ batashowroom(app)/batacampany/bata/model/raw_material_category.py
@@ -16,7 +16,22 @@ class Raw_Product_category(models.Model):
     Rexene = models.IntegerField(max_length=50,default=None)
     Clips = models.IntegerField(max_length=50,default=None)
     Color = models.IntegerField(max_length=50,default=None)
-    Price = models.IntegerField(max_length=50,default=None)
+    Price_Polyurethane = models.IntegerField(max_length=50,default=None)
+    Price_Rubber = models.IntegerField(max_length=50,default=None)
+    Price_Dye = models.IntegerField(max_length=50,default=None)
+    Price_Packaging_Material = models.IntegerField(max_length=50,default=None)
+    Price_GUM = models.IntegerField(max_length=50,default=None)
+    Price_PVC_Sole = models.IntegerField(max_length=50,default=None)
+    Price_TPR = models.IntegerField(max_length=50,default=None)
+    Price_Hard_Thread = models.IntegerField(max_length=50,default=None)
+    Price_Rexene = models.IntegerField(max_length=50,default=None)
+    Price_Clips = models.IntegerField(max_length=50,default=None)
+    Price_Color = models.IntegerField(max_length=50,default=None)
+
+
+
+
+
     product=models.ForeignKey(Product, on_delete=models.CASCADE, default=0)
     raw_product=models.ForeignKey(Raw_Product, on_delete=models.CASCADE, default=0)
 
